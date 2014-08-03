1e26353cda26b8b75253f1182aed930ccb945499
Kurt Lakin <kurtlakin@gmail.com>
2014-08-03 12:33:58 -0700

    First step with only one table. This code commited after basic bake of one table.

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'hsadKJHkjeh38hkjHFKJh43hjkfhjkdhfj34');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '546874213213164981015016510');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
