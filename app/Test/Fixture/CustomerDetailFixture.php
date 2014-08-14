35b55996cf2584075a6fec8aeded955dbbe389dc
Kurt Lakin <kurtlakin@gmail.com>
2014-08-14 13:35:21 -0700

    Created model for customerDetails table using Cake Bake.

---
+++ app/Test/Fixture/CustomerDetailFixture.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * CustomerDetailFixture
+ *
+ */
+class CustomerDetailFixture extends CakeTestFixture {
+
+/**
+ * Table name
+ *
+ * @var string
+ */
+	public $table = 'customerDetails';
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+		'customer_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'first_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'middle_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'last_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'birthdate' => array('type' => 'date', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'customer_id' => 1,
+			'created' => '2014-08-14 13:32:53',
+			'modified' => '2014-08-14 13:32:53',
+			'first_name' => 'Lorem ipsum dolor sit amet',
+			'middle_name' => 'Lorem ipsum dolor sit amet',
+			'last_name' => 'Lorem ipsum dolor sit amet',
+			'birthdate' => '2014-08-14'
+		),
+	);
+
+}
